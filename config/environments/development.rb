d9a012b77925ebe1f59cfd25a5d72abb6d2bb189
David Elì <davidelimg1@gmail.com>
2020-02-22 00:24:19 -0600

    Disabled pop up bullet alerts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.after_initialize do
     Bullet.enable        = true
-    Bullet.alert         = true
+    Bullet.alert         = false
     Bullet.bullet_logger = true
     Bullet.console       = true
     Bullet.rails_logger  = true
