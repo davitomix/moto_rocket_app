988f590e36f0b06f542c9c460211664f3a8f3656
David Elì <davidelimg1@gmail.com>
2020-02-25 10:01:41 -0600

    Modeling users, added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_prefix: 'auth'
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
