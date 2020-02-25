57eb97c9806c0812dacb14521ce71cf5a4ede773
David Elì <davidelimg1@gmail.com>
2020-02-25 14:35:37 -0600

    devise routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path_prefix: 'auth'
+  resources :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
