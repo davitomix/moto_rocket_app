3527e34103bd9740756d30ce18498b1f6c7602e9
David Elì <davidelimg1@gmail.com>
2020-02-25 21:38:07 -0600

    Added users signup, sessions with devise updated form views

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_02_25_154721) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
-User.create!(username: 'Example User',
-  email: 'example@railstutorial.org',
+User.create!(email: 'example@railstutorial.org',
   password: 'foobar',
   password_confirmation: 'foobar')
 
 49.times do |n|
-name  = Faker::Name.name
 email = "example-#{n + 1}@railstutorial.org"
 password = 'password'
-User.create!(username: name,
-   email: email,
+User.create!(email: email,
    password: password,
    password_confirmation: password)
 end
