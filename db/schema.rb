f1ed48b40fdacf38125967eeffca5219fae73f39
David Elì <davidelimg1@gmail.com>
2020-02-25 20:01:57 -0600

    Improved users table

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,9 @@ ActiveRecord::Schema.define(version: 2020_02_25_154721) do
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
