d4bf9214cc587ac3af665a945290d6b8f84d21c1
David Elì <davidelimg1@gmail.com>
2020-02-26 17:01:10 -0600

    Added username to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:new_user_session, keys: [:email, :username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email])
   end
 end
