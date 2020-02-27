c83cdd2154d86adf73ed18a49053d025370ced18
David Elì <davidelimg1@gmail.com>
2020-02-27 17:11:26 -0600

    Added users index and pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user
-  
+  before_action :correct_user, only: %i[edit update]
+
   def index
-    @users = User.all
+    @users = User.all.paginate(page: params[:page], per_page: 10)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-User.create!(email: 'example@railstutorial.org',
+User.create!(username: "user",
+  email: 'example@railstutorial.org',
   password: 'foobar',
   password_confirmation: 'foobar')
 
 49.times do |n|
-email = "example-#{n + 1}@railstutorial.org"
-password = 'password'
-User.create!(email: email,
-   password: password,
-   password_confirmation: password)
+  username = "user-#{n + 1}"
+  email = "example-#{n + 1}@railstutorial.org"
+  password = 'password'
+  User.create!(
+    username: username,
+    email: email,
+    password: password,
+    password_confirmation: password)
 end
