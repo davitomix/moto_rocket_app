96cae60af993501130d7e373f8673de14369dddf
David Elì <davidelimg1@gmail.com>
2020-02-27 22:16:34 -0600

    Adding testing to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
+  before_save :downcase_username
+  validates :username, presence: true, uniqueness: true, length: { minimum: 6, maximum: 15 }, format: { with: /\A[a-z0-9]+\z/ }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
   # attr_accessor :username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, length: { maximum: 60 }
+  private
+
+  # Converts email to all lower-case.
+  def downcase_username
+    self.username = username.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,63 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+RSpec.describe User, type: :model do
+
+  let(:user) do
+    User.create!(username: 'Foobar', email: 'foobar@bar.com',
+                 password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    user.username = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an email' do
+    user.email = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'must have a valid email' do
+    user.email = 'foobar@foo,bar'
+    expect(user).to_not be_valid
+  end
+
+  it 'must contain a password' do
+    user.password = ''
+    user.password_confirmation = ''
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if have a username > 15' do
+    user.username = 'a' * 20
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if it have uppercase letters' do
+    user.username = 'NiLl'
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid if it is nill' do
+    user.username = nil
+    expect(user).to_not be_valid
+  end
+
+  describe 'uniqueness' do
+    it 'is not valid a duplicated username' do
+      user = User.create!(username: 'Foobar', email: 'foobar@bar.com',
+        password: 'foobar', password_confirmation: 'foobar')
+      expect(user).to be_valid
+      user_clone = User.create!(username: 'Foobar2', email: 'foobar2@bar.com',
+        password: 'foobar2', password_confirmation: 'foobar2')
+      user_clone.username = 'foobar'
+      expect(user_clone).to_not be_valid
+    end
+  end
+end
