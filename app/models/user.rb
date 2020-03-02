44735cbf01d2c149b7af43ffb135b1daba0bf96d
David Elì <davidelimg1@gmail.com>
2020-03-02 15:20:38 -0600

    Fixed some lint issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
-  has_many :followers, through: :passive_relationships, source: :follower  
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
   default_scope -> { order(created_at: :desc) }
 
   def feed
