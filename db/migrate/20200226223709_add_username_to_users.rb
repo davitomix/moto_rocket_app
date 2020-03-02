073654c56d7653779fa6cd6ef6fc21ba37bfdbf7
David Elì <davidelimg1@gmail.com>
2020-03-02 12:56:15 -0600

    Heroku db configuried and working

--- db/migrate/20200226223709_add_username_to_users.rb
+++ db/migrate/20200226223709_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :username, :string
+    add_column :users, :username, :string, null: false, default: ""
   end
-  # add_index :users, :username, unique: true
+  add_index :users, :username, unique: true
 end
