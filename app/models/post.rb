b88a52fb20bcc93027f676f0ce31a5e27af16a4b
David Elì <davidelimg1@gmail.com>
2020-03-02 10:52:38 -0600

    Implemented image rezising and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,14 @@ class Post < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :content, presence: true, length: { in: 1..400 }
   validates :user_id, presence: true
-
+  validate :picture_size
+  
   private
 
-  # # Validates the size of an uploaded picture.
-  # def picture_size
-  #   return unless picture.size > 5.megabytes
+  # Validates the size of an uploaded picture.
+  def picture_size
+    return unless picture.size > 5.megabytes
 
-  #   errors.add(:picture, 'should be less than 5MB')
-  # end
+    errors.add(:picture, 'should be less than 5MB')
+  end
 end
