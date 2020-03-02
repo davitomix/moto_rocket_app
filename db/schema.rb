67bfd705e45ef0856378293facf826beea6d1dea
David Elì <davidelimg1@gmail.com>
2020-03-02 13:24:48 -0600

    Added index to usernames

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_152452) do
+ActiveRecord::Schema.define(version: 2020_03_02_192128) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_152452) do
     t.string "username", default: "", null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
